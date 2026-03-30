8595c19c18d5cb95096d75c194daf7c7498eb777
Lennart Wisbar <lewisbar.git@gmail.com>
2026-03-30 18:14:29 +0200

    Add `Pistol`

---
+++ Sources/Decorator Pattern/Pistol.swift
@@ -0,0 +1,12 @@
+//
+//  Pistol.swift
+//  Decorator Pattern
+//
+//  Created by Lennart Wisbar on 30.03.26.
+//
+
+class Pistol {
+    func shoot(_ direction: String) {
+        print("Shooting pistol to the \(direction). Bang!")
+    }
+}
