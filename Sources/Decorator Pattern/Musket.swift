c47fc70931f21f57cf5d03ef0b4b4054ba6032e1
Lennart Wisbar <lewisbar.git@gmail.com>
2026-03-30 18:15:05 +0200

    Add `Musket`

---
+++ Sources/Decorator Pattern/Musket.swift
@@ -0,0 +1,12 @@
+//
+//  Musket.swift
+//  Decorator Pattern
+//
+//  Created by Lennart Wisbar on 30.03.26.
+//
+
+class Musket {
+    func shoot(_ direction: String) {
+        print("Shooting musket to the \(direction). Bang!")
+    }
+}
