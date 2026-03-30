c330b98fdcf95fd9cdf3c474c32ca57b10fa8c9c
Lennart Wisbar <lewisbar.git@gmail.com>
2026-03-30 17:43:58 +0200

    Add `Cannon`

---
+++ Sources/Decorator Pattern/Cannon.swift
@@ -0,0 +1,12 @@
+//
+//  Cannon.swift
+//  Decorator Pattern
+//
+//  Created by Lennart Wisbar on 30.03.26.
+//
+
+class Cannon {
+    func shoot(_ direction: String) {
+        print("Shooting cannon to the \(direction). Boom!")
+    }
+}
